f0f7cee89af570637b5cfe0e74bb271e6d7ec2ac
ldoublewood <ldoublewood@gmail.com>
2019-12-05 20:47:14 +0800

    merge from docker-private-v1

--- cli/cmd.go
+++ cli/cmd.go
@@ -2,9 +2,11 @@ package cli
 
 import (
 	"context"
+	"github.com/multiformats/go-multiaddr"
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	logging "github.com/ipfs/go-log"
@@ -74,6 +76,22 @@ func GetRawAPI(ctx *cli.Context, repoFlag string) (string, http.Header, error) {
 	return "ws://" + addr + "/rpc/v0", headers, nil
 }
 
+func getAPIByAddrToken(strma string, token string) (string, http.Header, error) {
+	strma = strings.TrimSpace(strma)
+	ma, err := multiaddr.NewMultiaddr(strma)
+	if err != nil {
+		return "", nil, err
+	}
+	_, addr, err := manet.DialArgs(ma)
+	if err != nil {
+		return "", nil, err
+	}
+	var headers http.Header
+	headers = http.Header{}
+	headers.Add("Authorization", "Bearer "+ token)
+	return "ws://" + addr + "/rpc/v0", headers, nil
+}
+
 func GetAPI(ctx *cli.Context) (api.Common, jsonrpc.ClientCloser, error) {
 	f := "repo"
 	if ctx.String("storagerepo") != "" {
@@ -89,7 +107,20 @@ func GetAPI(ctx *cli.Context) (api.Common, jsonrpc.ClientCloser, error) {
 }
 
 func GetFullNodeAPI(ctx *cli.Context) (api.FullNode, jsonrpc.ClientCloser, error) {
-	addr, headers, err := GetRawAPI(ctx, "repo")
+	var addr string
+	var headers http.Header
+	var err error
+	fullnode := ctx.String("fullnode-api")
+	if len(fullnode) == 0 {
+		addr, headers, err = GetRawAPI(ctx, "repo")
+	} else {
+		token := ctx.String("fullnode-token")
+		if len(token) == 0 {
+			return nil, nil, xerrors.Errorf("Fullnode token should be provided")
+		}
+		addr, headers, err = getAPIByAddrToken(fullnode, token)
+	}
+
 	if err != nil {
 		return nil, nil, err
 	}
