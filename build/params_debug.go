8ae457d573e7b9d8001505c3fded99e9e93feae8
ldoublewood <ldoublewood@gmail.com>
2020-09-16 19:29:58 +0800

    fix listen-port

--- build/params_debug.go
+++ build/params_debug.go
@@ -1,10 +1,18 @@
 // +build debug
-
 package build
+import (
+     "github.com/filecoin-project/specs-actors/actors/builtin/miner"
+     "github.com/filecoin-project/go-state-types/abi"
+
+)
 
 func init() {
-	InsecurePoStValidation = true
-	BuildType |= BuildDebug
+     InsecurePoStValidation = true
+     BuildType |= BuildDebug
+     miner.SupportedProofTypes = map[abi.RegisteredSealProof]struct{}{
+	     abi.RegisteredSealProof_StackedDrg512MiBV1: {},
+     }
 }
+
 
 // NOTE: Also includes settings from params_2k
